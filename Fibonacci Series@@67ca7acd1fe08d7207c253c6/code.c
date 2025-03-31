18f0b033716a5cd993b64de0fd2fd5c38d065885
Bhavisha <bhavisha0996.becse24@chitkara.edu.in>
2025-04-01 01:52:13 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,14 +3,14 @@
 void fibonacciSeries(int n){
     int a=0 , b=1, next;
     if(n>=1 ){
-        printf("%d",a);
+        printf("%d ",a);
     }
     if(n>=2){
-        printf("%d",b);
+        printf("%d ",b);
     }
     for(int i=3 ; i<=n ; i++){
         next=a+b;
-        printf("%d", next);
+        printf("%d ", next);
         a=b;
         b=next;
         
