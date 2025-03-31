5dfcd22533177983c13422e7252a1dc9737a32bf
Bhavisha <bhavisha0996.becse24@chitkara.edu.in>
2025-04-01 01:33:58 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+#include <stdio.h>
+int fibonacciSeries(int n){
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    else if{
+        return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    }
+}
