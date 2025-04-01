9003287ea20943e80ab052b115e0523813613bac
Bhavisha <bhavisha0996.becse24@chitkara.edu.in>
2025-04-01 10:41:31 +0530

    Solution to Binary Search on a Sorted Array

--- Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -1,41 +1,41 @@
 // Your code here...
-// #include <stdio.h>
-// int binarySearch(int arr[], int n ,int target){
-//     int high ; int low ; int mid;
-//     low = 0;
-//     high = n - 1;
-//     while(low<=high){
-//     mid = (low+high)/2;
-//     if(target == arr[mid]){
-//         return mid;
-//     }
-//     if(target > arr[mid]){
-//         low= mid+1;
-//     }
-//     else{
-//         high= mid -1;
-//     }
-//     }
-//     return -1;
-// }
-
 #include <stdio.h>
-
-int binarySearch(int arr[],int n, int target){
+int binarySearch(int arr[], int n ,int target){
     int high ; int low ; int mid;
     low = 0;
-    high = n-1;
+    high = n - 1;
     while(low<=high){
-        mid =(low+high)/2;
-        if(target == arr[mid]){
-            return 1;
-        }
-        if(target > arr[mid]){
-            low = mid +1;
-        }
-        else if(target < arr[mid]){
-            high = mid -1;
-        }
+    mid = (low+high)/2;
+    if(target == arr[mid]){
+        return mid;
+    }
+    if(target > arr[mid]){
+        low= mid+1;
+    }
+    else{
+        high= mid -1;
+    }
     }
     return -1;
 }
+
+// #include <stdio.h>
+
+// int binarySearch(int arr[],int n, int target){
+//     int high ; int low ; int mid;
+//     low = 0;
+//     high = n-1;
+//     while(low<=high){
+//         mid =(low+high)/2;
+//         if(target == arr[mid]){
+//             return 1;
+//         }
+//         if(target > arr[mid]){
+//             low = mid +1;
+//         }
+//         else if(target < arr[mid]){
+//             high = mid -1;
+//         }
+//     }
+//     return -1;
+// }
